b722a121192da3472e705435e486d6c4fca5dfe9
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-02-26 16:18:51 -0800

    Add MovieTest stubs

---
+++ Java Project/src/movies/MovieTest.java
@@ -0,0 +1,64 @@
+package movies;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class MovieTest {
+
+	@Test
+	void testConstructor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetYear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDirector() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetWriter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareToGreater() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareToSmaller() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsTrue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsFalse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
