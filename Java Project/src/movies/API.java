0bd44c9bb3afadd28e1e956bc879007fb4f9ea08
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-02-26 16:18:42 -0800

    Add API class

---
+++ Java Project/src/movies/API.java
@@ -0,0 +1,32 @@
+package movies;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class API {
+
+	public Movie parseFrom(Document xml) {
+		xml.getDocumentElement().normalize();
+		NodeList list = xml.getElementsByTagName("movie");
+
+		String name = "";
+		int year = 0;
+		String director = "";
+		String writer = "";
+
+		for (int temp = 0; temp < list.getLength(); temp++) {
+			Node node = list.item(temp);
+
+			if (node.getNodeType() == Node.ELEMENT_NODE) {
+				Element movie = (Element) node;
+				name = movie.getAttribute("title");
+				director = movie.getAttribute("director");
+				writer = movie.getAttribute("writer");
+			}
+		}
+		return new Movie(name, year, director, writer);
+	}
+
+}
